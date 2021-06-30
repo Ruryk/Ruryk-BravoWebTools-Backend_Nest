4e1ecbc3f93293d0e8ce7db04df6305a5d064516
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-06-30 11:50:51 +0300

    add authentication user

--- src/authentication/controller/authentication.controller.ts
+++ src/authentication/controller/authentication.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationDto } from '../dto/authentication.dto';
 import { AuthenticationService } from '../service/authentication.service';
 import { VerificationDto } from '../dto/verification.dto';
+import { LoginDto } from '../dto/login.dto';
+import { CheckCodeDto } from '../dto/checkCode.dto';
+import { LoginTokenDto } from '../dto/loginToken.dto';
 
 @Controller('authentication')
 export class AuthenticationController {
-  constructor(private readonly authenticationService: AuthenticationService) {}
+  constructor(private readonly authenticationService: AuthenticationService) {
+  }
 
   @Post('check-user')
   @HttpCode(HttpStatus.OK)
@@ -15,7 +19,32 @@ export class AuthenticationController {
 
   @Post('verification')
   @HttpCode(HttpStatus.OK)
-  async verificationUser(@Body() verification: VerificationDto): Promise<boolean> {
+  async verificationUser(
+    @Body() verification: VerificationDto,
+  ): Promise<boolean> {
     return this.authenticationService.verificationUser(verification.email);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async loginUser(
+    @Body() login: LoginDto,
+  ): Promise<boolean> {
+    return this.authenticationService.loginUser(login.email, login.code);
+  }
+
+  @Post('login-token')
+  @HttpCode(HttpStatus.OK)
+  async loginWithToken(@Body() loginToken: LoginTokenDto): Promise<boolean> {
+    return this.authenticationService.loginWithToken(loginToken.token);
+  }
+
+  @Post('check-code')
+  @HttpCode(HttpStatus.OK)
+  async checkCode(@Body() checkCode: CheckCodeDto): Promise<boolean> {
+    return this.authenticationService.checkCode(
+      checkCode.email,
+      checkCode.code,
+    );
   }
 }
--- src/authentication/service/authentication.service.ts
+++ src/authentication/service/authentication.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { HttpCode, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as nodemailer from 'nodemailer';
+
 import {
   Authentication,
   AuthenticationDocument,
@@ -21,22 +23,83 @@ export class AuthenticationService {
   }
 
   async verificationUser(email: string): Promise<any> {
-    const user = await this.authenticationModel.findOne({ email });
-    let code = '';
-    if (user) {
-      code = this.generateCodeVerification();
-      await this.authenticationModel
-        .updateOne({ email }, { $set: { code } })
-        .exec();
-      this.sendCodeToEmail(code);
-      return true;
-    }
-    return false;
+    return new Promise(async (resolve, reject) => {
+      const user = await this.authenticationModel.findOne({ email });
+      let code = '';
+      if (user) {
+        code = this.generateCodeVerification(6);
+        const writeToDbStatus = await this.authenticationModel
+          .findOneAndUpdate({ email }, { $set: { code } })
+          .exec();
+        if (writeToDbStatus) {
+          const sendStatus: any = await this.sendCodeToEmail(code, email);
+          resolve({ status: sendStatus.response });
+        }
+        reject({ status: 401 });
+      }
+      reject({ status: 401 });
+    });
   }
 
-  generateCodeVerification(): string {
+  async loginUser(email: string, code: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const user = await this.authenticationModel.findOne({ email });
+      if (user && user.code === code) {
+        const access_token = this.generateHash(20);
+        const writeToDbStatus = await this.authenticationModel
+          .findOneAndUpdate(
+            { email },
+            { $set: { token: access_token, code: '' } },
+          )
+          .exec();
+        if (writeToDbStatus) {
+          resolve({ access_token });
+        }
+        resolve({ status: 401, message: 'Incorrect write to db' });
+      }
+      reject({ status: 401, message: 'Incorrect code' });
+    });
+  }
+
+  async loginWithToken(token: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const user = await this.authenticationModel.findOne({ token });
+      if (user && user.token === token) {
+        const access_token = this.generateHash(20);
+        const writeToDbStatus = await this.authenticationModel
+          .findOneAndUpdate(
+            { token },
+            { $set: { token: access_token, code: '' } },
+          )
+          .exec();
+        if (writeToDbStatus) {
+          resolve({ token: access_token });
+        }
+        resolve({ status: false });
+      }
+      reject({ status: false });
+    });
+  }
+
+  async checkCode(email: string, code: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      const user = await this.authenticationModel.findOne({ email });
+      if (user && user.code === code) {
+        resolve({ status: 200 });
+      }
+      reject({ status: 401, message: 'Incorrect code' });
+    });
+  }
+
+  generateCodeVerification(length: number): string {
+    return Math.floor(Math.random() * 1000001)
+      .toString()
+      .padStart(length, '0');
+  }
+
+  generateHash(length: number): string {
     let str = '';
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < length; i++) {
       const rand = Math.floor(
         Math.random() * (symbolGeneratorString.length + 1),
       );
@@ -45,7 +108,24 @@ export class AuthenticationService {
     return str;
   }
 
-  sendCodeToEmail(code: string): void {
-
+  async sendCodeToEmail(code: string, email: string): Promise<void> {
+    // create reusable transporter object using the default SMTP transport
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // use SSL
+      auth: {
+        user: 'mr.ivent6@gmail.com',
+        pass: '1598753ivent',
+      },
+    });
+    // send mail with defined transport object
+    return transporter.sendMail({
+      from: 'bravoWebTool@domain.com',
+      to: email,
+      subject: 'Confirm Email',
+      text: 'Please confirm your email',
+      html: '<p>Please confirm your email</p><p>Code: ' + code + '</p>',
+    });
   }
 }
